656c8bc448b8bc4c581b53e8115819588408939e
Daniel Possamai <daniel.possamai20@gmail.com>
2024-05-23 21:02:10 -0300

    Quase finalizado, ja adicionada função para trocar tema, falta resolver problema do hover que aumenta o tamanho do botao

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 let btnTheme = document.getElementById('switch');
+let meuHtml = document.querySelector('html');
+
+meuHtml.classList.toggle('light');
 
 btnTheme.addEventListener('click', () => {
-    
+    meuHtml.classList.toggle('light');
 })
