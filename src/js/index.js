149e2591426a5cc0b875e642392336ef0ca8c9cf
Daniel Possamai <daniel.possamai20@gmail.com>
2024-05-24 10:37:27 -0300

    Atualizado e adicionado logica para trocar imagem do profile quando mudar o tema

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,16 @@
 let btnTheme = document.getElementById('switch');
 let meuHtml = document.querySelector('html');
+let profileImg = document.querySelector('#profile img');
 
 meuHtml.classList.toggle('light');
 
 btnTheme.addEventListener('click', () => {
     meuHtml.classList.toggle('light');
-})
+
+    if(meuHtml.classList.contains('light')){
+        profileImg.src = './src/images/imgPerson.png';
+    }else{
+        profileImg.src = './src/images/personlighht.png'
+    }
+});
+
